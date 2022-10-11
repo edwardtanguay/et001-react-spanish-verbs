be8bb9cbaf4fcae2f292d6e5c9357ca6af716ba4
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-12 00:20:42 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Spanish Verb Quiz</h1>
+      <p>testing</p>
+    </div>
+  )
+}
+
+export default App
